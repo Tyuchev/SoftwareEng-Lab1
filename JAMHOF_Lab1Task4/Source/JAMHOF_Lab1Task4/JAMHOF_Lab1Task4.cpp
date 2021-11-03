d4121c45e51efac3afd0d2b65efc510838014c76
Tyuchev <tyuchevv@gmail.com>
2021-11-03 14:30:31 +0100

    Creation of Task 4 & removal of Task 3
    Task 3 was having issues with clashes of git LTS and Unreal umaps - Will be recreated

---
+++ JAMHOF_Lab1Task4/Source/JAMHOF_Lab1Task4/JAMHOF_Lab1Task4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JAMHOF_Lab1Task4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JAMHOF_Lab1Task4, "JAMHOF_Lab1Task4" );
---
+++ JAMHOF_Lab1Task4/Source/JAMHOF_Lab1Task4/JAMHOF_Lab1Task4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JAMHOF_Lab1Task4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JAMHOF_LAB1TASK4_API AJAMHOF_Lab1Task4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
